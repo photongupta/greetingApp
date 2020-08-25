9b717e397545726490366ad538b44dfae688a45c
Rashmi Gupta <rashmi224141@gmail.com>
2020-08-25 08:53:38 +0530

    added feature of greeting a person

---
+++ src/greet.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Greet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', isSubmit: false};
+    this.updateName = this.updateName.bind(this);
+    this.toggleSubmit = this.toggleSubmit.bind(this);
+  }
+
+  updateName({target}) {
+    this.setState(() => ({name: target.value}));
+  }
+
+  toggleSubmit() {
+    this.setState((state) => ({isSubmit: !state.isSubmit}));
+  }
+
+  render() {
+    let nameBox = <input placeholder="Name" onKeyUp={this.updateName}></input>;
+    let buttonText = 'Say hello';
+
+    if (this.state.isSubmit) {
+      buttonText = 'Enter other name';
+      nameBox = <p>hello {this.state.name}</p>;
+    }
+
+    return (
+      <div>
+        {nameBox}
+        <button onClick={this.toggleSubmit}>{buttonText}</button>
+      </div>
+    );
+  }
+}
+
+export default Greet;
